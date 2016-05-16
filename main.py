b070c15e3aaa81cf9554fb8d421f61b67c29b130
sky5497 <peter860611@yahoo.com>
2016-05-16 16:01:15 +0800

    1
    1

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ jinja_env = jinja2.Environment(
 
 import webapp2
 from gppgle.appengine.api import users
+from models import Note
 class MainHandler(webapp2.RequestHandler):
     def get(self):
     user = users.get_current_user()
@@ -35,18 +36,22 @@ class MainHandler(webapp2.RequestHandler):
     else:
         login_url = users.create_login_url(self.request.uri)
         self.redirect(login_url)
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
         
     def post(self):
         user = users.get_current_user()
         if user is None:
             self.error(401)
+        
+
         self._create_note(user)
         logout_url = users.create_logout_url(self.request.uri)
         template_context = {
             'user':user.nickname(),
             'logout_url':logout_url,
-            'note_title':self.request.get('title'),
-            'note_content':self.request.get('content'),}
+        }
 
     def _render_template(self,template_name,context=None):
         if context is None:
@@ -54,13 +59,23 @@ class MainHandler(webapp2.RequestHandler):
         user = users.get_current_user()
         ancestor_key = ndb.key("User",user.nickname())
         qry = Note.owner_query(ancestor_key)
-        context{"notes"} = qry.fetch()
+        context["notes"] = qry.fetch()
         template = jinja_env.get_template(template_name)
         return template.render(context)
     
     self.response.out.write(
         self._render_template('main.html',template_context))
-    
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+            
+    @ndb.transactional
+    def _create_note(self,user):
+        
+        note = Note(parent=ndb.Key("User",user.nickname()),
+                    title=self.request.get('title'),
+                    content=self.request.get('content'))
+        note.put()
+        item_titles = self.request.get('checklist_items').split(',')
+        for item_title in item_titles:
+            item = CheckListItem(parent=note.key,title=item_title)
+            item.put()
+            note.checklist_items.append(item.key)
+        note.put()
